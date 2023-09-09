ac5e36a598a89953cd8b1028d421d33d583628e6
Oydual3 <claucondor@gmail.com>
2023-09-09 15:39:41 -0400

    feat: add newWaves entity

--- src/entities/newWav3s.ts
+++ src/entities/newWav3s.ts
@@ -1,1 +1,28 @@
-type NewWav3s = {};
+import { Timestamp } from "@google-cloud/firestore";
+
+type DistributionDto = {
+  type: string;
+  duration?: number;
+  raffleEndDate?: Date;
+};
+
+type NewWav3 = {
+  id?: string;
+  externalId: string;
+  action: string;
+  socialGraph: string;
+  distribution: DistributionDto;
+  status: string;
+  totalReward?: number;
+  rewardPerZurfer: number;
+  currency: string;
+  minimumFollowersAction: number;
+  goalOfAction: number;
+  ownedBy: string;
+  handle: string;
+  contractVersion: number;
+  specialConditions: any;
+  createdAt?: Timestamp;
+};
+
+export { NewWav3, DistributionDto };
