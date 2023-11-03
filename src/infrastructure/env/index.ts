dfb8c253d57165659a971b0e2f4e0c68948b5038
Oydual3 <claucondor@gmail.com>
2023-11-03 18:32:39 -0400

    feat: add env variables for tests

--- src/infrastructure/env/index.ts
+++ src/infrastructure/env/index.ts
@@ -1,1 +1,10 @@
-export const LENS_API_URL = "https://api.lens.dev/";
+import * as dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Define and export your environment variables
+export const LENS_API_URL = process.env.LENS_API_URL;
+export const WALLET_PK = process.env.WALLET_PK;
+export const PROFILE_ID = process.env.PROFILE_ID;
+export const PROFILE_ADRESS = process.env.PROFILE_ADDRESS;
