888e2785e103d537c496201b29d7988f49001a6a
Oydual3 <claucondor@gmail.com>
2023-11-03 21:05:19 -0400

    feat: add gedChallenge function

--- src/utils/lens-scripts-tests/get-challengue.ts
+++ src/utils/lens-scripts-tests/get-challengue.ts
@@ -1,12 +1,19 @@
-async function getChallenge(client: GraphQLClient, signedBy: string, forProfile: string): Promise<string> {
+import { GraphQLClient } from "graphql-request";
+import { CHALLENGE_QUERY } from "./querys";
+
+async function getChallenge(
+  client: GraphQLClient,
+  signedBy: string,
+  forProfile: string
+): Promise<string> {
   try {
-    const response = await client.request(CHALLENGE_QUERY, {
+    const response = (await client.request(CHALLENGE_QUERY, {
       signedBy,
       for: forProfile,
-    });
+    })) as any;
 
     return response.challenge.text;
-  } catch (error) {
+  } catch (error: any) {
     throw new Error(`Error while getting challenge: ${error.message}`);
   }
 }
