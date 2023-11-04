b9cc2e26d5f4c2009fa9114646a22ab81a669740
Oydual3 <claucondor@gmail.com>
2023-11-03 22:33:27 -0400

    fix : onst AUTHENTICATE_MUTATION

--- src/utils/lens-scripts-tests/querys/authenticate-mutation.ts
+++ src/utils/lens-scripts-tests/querys/authenticate-mutation.ts
@@ -1,5 +1,5 @@
 export const AUTHENTICATE_MUTATION = `
-  mutation Authenticate($id: String, $signature: String) {
+  mutation Authenticate($id: ChallengeId!, $signature: Signature!) {
     authenticate(request: { id: $id, signature: $signature }) {
       accessToken
       refreshToken
