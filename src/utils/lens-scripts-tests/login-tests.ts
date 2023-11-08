0534d0b63e69d3314c83a52f7b27398e303e6101
Oydual3 <claucondor@gmail.com>
2023-11-08 18:36:31 -0400

    testint lens Sdk v2

--- src/utils/lens-scripts-tests/login-tests.ts
+++ src/utils/lens-scripts-tests/login-tests.ts
@@ -7,35 +7,47 @@ import {
   PROFILE_ID,
   WALLET_PK,
 } from "../../infrastructure/env";
+import { LensClient, production } from "@lens-protocol/client";
+import {
+  ImageSetFragment,
+  ProfileFragment,
+} from "@lens-protocol/client/dist/declarations/src";
+import { lens } from "../consts/lens";
 
 const GRAPHQL_API_URL = "https://api-v2.lens.dev/";
 
 const client = new GraphQLClient(GRAPHQL_API_URL);
+const lensClient = new LensClient({
+  environment: production,
+});
 
 async function authenticateUser() {
-  const signedBy = PROFILE_ADRESS;
-  const forProfile = PROFILE_ID;
+  const signedBy = PROFILE_ADRESS as string;
+  const forProfile = PROFILE_ID as string;
 
   try {
     console.log("Signed by:", signedBy);
     console.log("For profile:", forProfile);
-    const challengeData = await getChallenge(
-      client,
-      signedBy as string,
-      forProfile as string
-    );
-    console.log("Challenge text:", challengeData);
 
-    const wallet = new Wallet(WALLET_PK as string); // Reemplaza con tu clave privada real
-    const signature = await wallet.signMessage(challengeData.text);
+    const { id, text } = await lensClient.authentication.generateChallenge({
+      signedBy,
+      for: forProfile,
+    });
+    console.log("Challenge text:", text);
 
-    const tokens = await authenticateWithChallenge(
-      client,
-      challengeData.id,
-      signature
+    const wallet = new Wallet(WALLET_PK as string);
+    const signature = await wallet.signMessage(text);
+
+    await lensClient.authentication.authenticate({
+      id,
+      signature,
+    });
+
+    console.log(
+      "Access Token:",
+      (await lensClient.authentication.getAccessToken()).unwrap()
     );
-    console.log("Access Token:", tokens.accessToken);
-    console.log("Refresh Token:", tokens.refreshToken);
+    console.log(await lensClient.authentication.fetch());
   } catch (error) {
     console.error(error);
   }
