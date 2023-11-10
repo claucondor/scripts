feec6af09524671d7de5d431fae317a131aee3ba
Oydual3 <claucondor@gmail.com>
2023-11-10 14:44:23 -0400

    add profile stats interface

---
+++ src/entities/feed/profile-stats.ts
@@ -0,0 +1,13 @@
+export interface ProfileStats {
+    id: string;
+    followers: number;
+    following: number;
+    comments: number;
+    posts: number;
+    mirrors: number;
+    quotes: number;
+    publications: number;
+    reactions: number;
+    reacted: number;
+    countOpenActions: number;
+  }
